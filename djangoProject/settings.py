135b4d348253f43cd96c57940d5a683cbdf96aa2
Batuhanfahri <batuhanfahri06@gmail.com>
2024-12-05 22:08:26 +0300

    whitenose

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
 INSTALLED_APPS = [
     'daphne',
+    'whitenoise',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
