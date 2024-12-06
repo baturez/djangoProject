50c59a659c8a016b0c297d1eb32d734f40ecf786
Batuhanfahri <batuhanfahri06@gmail.com>
2024-12-07 01:56:10 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # HTTP için Gunicorn'u başlat
-gunicorn --workers=3 --bind=0.0.0.0:8000 djangoProject.wsgi:application &
+gunicorn --workers=3 --bind=0.0.0.0:10000 djangoProject.wsgi:application &
 
 # WebSocket için Daphne'yi başlat
 daphne -p 8000 djangoProject.asgi:application
